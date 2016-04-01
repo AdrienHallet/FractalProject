d6206943b546d48314242708ec6e2894a37f5133
AdrienHalletUCL <adrien.hallet@student.uclouvain.be>
2016-04-01 15:31:01 +0200

    Update main.c naive thread version
    Un premier test fonctionnel d'une lecture threadée

--- main.c
+++ main.c
@@ -1,8 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "libfractal/fractal.h"
 #include "errorHandling.h"
+
+struct arg_readStruct{
+	const char *filename;
+	void *buffer;
+};
+
+char *strsep(char **stringp, const char *delim){
+	char *s;
+	const char *spanp;
+	int c, sc;
+	char *tok;
+
+	if ((s = *stringp) == NULL)
+	    return (NULL);
+	for (tok = s;;) {
+	    c = *s++;
+	    spanp = delim;
+	    do {
+		    if ((sc = *spanp++) == c) {
+			    if (c == 0)
+				    s = NULL;
+			    else
+				    s[-1] = 0;
+			    *stringp = s;
+			    return (tok);
+		    }
+	    } while (sc != 0);
+	}
+	/* NOTREACHED */
+}
+
+struct fractal *fractal_parse(char *line){
+	const char *delim = " ";
+	char **endptr;
+	long value;
+	char *name = strsep(&line, delim);
+
+	int width = (int) strtol(strsep(&line, delim), endptr, 10);
+	int height = (int) strtol(strsep(&line, delim), endptr, 10);
+	double a = (double) strtol(strsep(&line, delim), endptr, 10);
+	double b= (double) strtol(strsep(&line, delim), endptr, 10);
+	struct fractal *newfractal = fractal_new(name, width, height, a, b);
+
+	
+}
+
+int *readFile(const char *filename, struct fractal *(buffer)[]){
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int *iteration;
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	return -1;
+	while ((read = getline(&line, &len, fp)) != -1) {
+		if(read>1 && line[0]!='#'){ //ignore empty lines and comments
+		struct fractal *myfrac = fractal_parse(line);
+		iteration++;
+		}
+	}
+	free(line);
+	free(fp);
+	pthread_exit(iteration);
+}
 
 int main(int argc, const char * argv[])
 {
@@ -23,51 +89,29 @@ int main(int argc, const char * argv[])
 	if(handle_no_arguments(argc) < 0){
 		return 0;	
 	}
-	readFile();
-	
-}
-
-struct fractal *fractal_parse(char *line){
-	printf("%s\n", line);
-	const char *delim = " ";
-	char **endptr;
-	long value;
-	//printf("%s\n",strsep(&line, delim));
-	char *name = strsep(&line, delim);
-	printf("strsep success\n");
-	int width = (int) strtol(strsep(&line, delim), endptr, 10);
-	int height = (int) strtol(strsep(&line, delim), endptr, 10);
-	double a = (double) strtol(strsep(&line, delim), endptr, 10);
-	double b= (double) strtol(strsep(&line, delim), endptr, 10);
-	printf("Reached\n");
-	struct fractal *newfractal = fractal_new(name, width, height, a, b);
-
-	
-}
-
-int readFile(){
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
-	fp = fopen("inputFile", "r");
-	if (fp == NULL)
-	exit(EXIT_FAILURE);
-	while ((read = getline(&line, &len, fp)) != -1) {
-		if(read>1 && line[0]!='#'){ //ignore empty lines and comments
-		struct fractal *myfrac = fractal_parse(line);
-			int w,h,x,y;
-			for(x = 0; x < 800; x++){
-				for(y = 0; y < 800; y++){
-					fractal_compute_value(myfrac,x,y);
-				}
-
-			}
-			write_bitmap_sdl(myfrac, "Image.bmp");
-		}
+	int maxThreads = 10;
+	struct fractal *fractalBuffer[maxThreads];
+	int i;
+	for(i = 0; i < maxThreads; i++){
+		fractalBuffer[i] = malloc(sizeof(struct fractal));
+		fractalBuffer[i] = NULL;
 	}
-	free(line);
-	free(fp);
-	exit(EXIT_SUCCESS);
+	struct fractal *(*buffer)[] = &fractalBuffer;
+	const char *filename = "inputfile1";
+	pthread_t thread;
+	struct arg_readStruct args;
+	args.filename = filename;
+	args.buffer = (void *)buffer;
+	if(pthread_create(&thread, NULL, readFile, (void *)&args) == -1)
+		return EXIT_FAILURE;
+	printf("Thread created\n");
+	int *iteration;
+	if(pthread_join(thread, iteration))
+		return EXIT_FAILURE;
+	
+
+	for(i = 0; i < maxThreads; i++){
+		free(fractalBuffer[i]);
+	}
+	return 0;
 }
